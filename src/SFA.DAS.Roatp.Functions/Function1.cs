a24d9b511dd4294871fd3594e97dc1d13d2ad45b
Martin Lush <martin-hornsby@ntlworld.com>
2021-02-18 08:40:36 +0000

    Add initial configuration

--- src/SFA.DAS.Roatp.Functions/Function1.cs
+++ src/SFA.DAS.Roatp.Functions/Function1.cs
@@ -1,20 +1,40 @@
 using System;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SFA.DAS.Roatp.Functions.Configuration;
 
 namespace SFA.DAS.Roatp.Functions
 {
-    public static class Function1
+    public class Function1
     {
+        private readonly IConfiguration _configuration;
+        private readonly ApplyApiAuthentication _applyApiAuthentication;
+        private readonly QnaApiAuthentication _qnaApiAuthentication;
+        
+        public Function1(IConfiguration configuration, IOptions<ApplyApiAuthentication> applyApiAuthentication, IServiceProvider serviceProvider)
+        {
+            // NOTE: Here are several different ways to get config. Best practise is to use IOptions but I know some prefer IConfiguration
+            _configuration = configuration;
+            _applyApiAuthentication = applyApiAuthentication.Value;
+            _qnaApiAuthentication = serviceProvider.GetService<IOptions<QnaApiAuthentication>>().Value;
+        }
+
+
         [FunctionName("Function1")]
-        public static void Run([TimerTrigger("0 */5 * * * *", RunOnStartup = true)]TimerInfo myTimer, ILogger log)
+        public async Task Run([TimerTrigger("0 */5 * * * *", RunOnStartup = true)]TimerInfo myTimer, ILogger log)
         {
             if (myTimer.IsPastDue)
             {
-                log.LogInformation("c# Timer trigger function is running later than scheduled");
+                log.LogInformation("C# Timer trigger function is running later than scheduled");
             }
 
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            await Task.CompletedTask;
         }
     }
 }
