acd6eddca4b941ce1c6aec9a24739908967f014e
Martin Lush <martin-hornsby@ntlworld.com>
2021-02-22 09:10:19 +0000

    Add QnA & RoATP apply clients

---
+++ src/SFA.DAS.Roatp.Functions/Infrastructure/ApiClients/IQnaApiClient.cs
@@ -0,0 +1,12 @@
+﻿using SFA.DAS.QnA.Api.Types;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.Functions.Infrastructure.ApiClients
+{
+    public interface IQnaApiClient
+    {
+        Task<IEnumerable<Section>> GetAllSectionsForApplication(Guid applicationId);
+    }
+}
---
+++ src/SFA.DAS.Roatp.Functions/Infrastructure/Tokens/BearerTokenGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.Net.Http.Headers;
+
+namespace SFA.DAS.Roatp.Functions.Infrastructure.Tokens
+{
+    public static class BearerTokenGenerator
+    {
+        public static AuthenticationHeaderValue GenerateToken(string tenantId, string clientId, string clientSecret, string resourceId)
+        {
+            var authority = $"https://login.microsoftonline.com/{tenantId}";
+            var clientCredential = new ClientCredential(clientId, clientSecret);
+            var context = new AuthenticationContext(authority, true);
+            var result = context.AcquireTokenAsync(resourceId, clientCredential).GetAwaiter().GetResult();
+
+            return new AuthenticationHeaderValue("Bearer", result.AccessToken);
+        }
+    }
+}
