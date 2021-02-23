e40743b7ef0f281b8654324e4c6e6bfa91bc7e60
Martin Lush <martin-hornsby@ntlworld.com>
2021-02-23 13:10:03 +0000

    Add new RoATP type for extracting application answers

---
+++ src/SFA.DAS.Roatp.Functions/ApplyTypes/ExtractedApplication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SFA.DAS.Roatp.Functions.ApplyTypes
+{
+    public class ExtractedApplication
+    {
+        public Guid Id { get; set; }
+        public Guid ApplicationId { get; set; }
+        public DateTime ExtractedDate { get; set; }
+    }
+}
