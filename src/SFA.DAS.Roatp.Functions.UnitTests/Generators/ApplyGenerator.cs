92f55966ab12f167ca87494e880eb4d4924af9dd
Martin Lush <martin-hornsby@ntlworld.com>
2021-02-24 15:42:36 +0000

    Updated logic based on new EF navigation properties

--- src/SFA.DAS.Roatp.Functions.UnitTests/Generators/ApplyGenerator.cs
+++ src/SFA.DAS.Roatp.Functions.UnitTests/Generators/ApplyGenerator.cs
@@ -5,16 +5,17 @@ namespace SFA.DAS.Roatp.Functions.UnitTests.Generators
 {
     public static class ApplyGenerator
     {
-        public static Apply GenerateApplication(Guid applicationId, DateTime applictionSubmittedDate)
+        public static Apply GenerateApplication(Guid applicationId, string applicationStatus, DateTime? applicationSubmittedDate)
         {
             return new Apply
             {
                 ApplicationId = applicationId,
+                ApplicationStatus = applicationStatus,
                 ApplyData = new ApplyData
                 {
                     ApplyDetails = new ApplyDetails
                     {
-                        ApplicationSubmittedOn = applictionSubmittedDate
+                        ApplicationSubmittedOn = applicationSubmittedDate
                     }
                 }
             };
