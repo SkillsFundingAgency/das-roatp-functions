3ff86cd40d27aaae313f522d298dba5a908495f1
Martin Lush <martin-hornsby@ntlworld.com>
2021-05-27 08:55:11 +0100

    APR-2389 - Implemented Serializable so it can be used on the Service Bus appropraitely

--- src/SFA.DAS.Roatp.Functions/Requests/ApplyFileExtractRequest.cs
+++ src/SFA.DAS.Roatp.Functions/Requests/ApplyFileExtractRequest.cs
@@ -1,9 +1,12 @@
 using SFA.DAS.Roatp.Functions.ApplyTypes;
 using System;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
 
 namespace SFA.DAS.Roatp.Functions.Requests
 {
-    public class ApplyFileExtractRequest
+    [Serializable]
+    public class ApplyFileExtractRequest : ISerializable
     {
         public Guid ApplicationId { get; }
         public int SequenceNumber { get; }
@@ -21,5 +24,30 @@ namespace SFA.DAS.Roatp.Functions.Requests
             SequenceNumber = submittedApplicationAnswer.SequenceNumber;
             Filename = submittedApplicationAnswer.Answer;
         }
+
+        #region Serialization
+        // This is the serialization constructor.
+        // Satisfies rule CA2229: Implement serialization constructors
+        protected ApplyFileExtractRequest(SerializationInfo info, StreamingContext context)
+        {
+            ApplicationId = (Guid)info.GetValue(nameof(ApplicationId), typeof(Guid));
+            SequenceNumber = info.GetInt32(nameof(SequenceNumber));
+            SectionNumber = info.GetInt32(nameof(SectionNumber));
+            PageId = info.GetString(nameof(PageId));
+            QuestionId = info.GetString(nameof(QuestionId));
+            Filename = info.GetString(nameof(Filename));
+        }
+
+        [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.SerializationFormatter)]
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue(nameof(ApplicationId), ApplicationId);
+            info.AddValue(nameof(SequenceNumber), SequenceNumber);
+            info.AddValue(nameof(SectionNumber), SectionNumber);
+            info.AddValue(nameof(PageId), PageId);
+            info.AddValue(nameof(QuestionId), QuestionId);
+            info.AddValue(nameof(Filename), Filename);
+        }
+        #endregion
     }
 }
